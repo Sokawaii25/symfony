96c182c47af86ac0df03fc87d2fe05c318fba54c
Anthony HORNEC <ahornec@iutbayonne.univ-pau.fr>
2020-12-01 16:30:48 +0100

    création des liens du tp1 ?

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -9,12 +9,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStagesController extends AbstractController
 {
     /**
-     * @Route("/pro/stages", name="pro_stages")
+       * @Route("/", name="accueil")
      */
     public function index(): Response
     {
         return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'ProStagesController',
+            'controller_name' => 'Bienvenue sur la page d\'accueil de Prostages',
+        ]);
+    }
+
+    /**
+       * @Route("/entreprises", name="entreprises")
+     */
+    public function entreprises(): Response
+    {
+        return $this->render('pro_stages/index.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
+        ]);
+    }
+
+    /**
+       * @Route("/formations", name="formations")
+     */
+    public function formations(): Response
+    {
+        return $this->render('pro_stages/index.html.twig', [
+            'controller_name' => 'Cette page affichera la liste des formations de l\'IUT',
+        ]);
+    }
+
+    /**
+       * @Route("/stages/{id}", name="stages")
+     */
+    public function stages($id): Response
+    {
+        return $this->render('pro_stages/index.html.twig', [
+            'controller_name' => 'Cette page affichera le descriptif du stage ayant pour identifiant '.$id,
         ]);
     }
 }
