819a739c17f9f52d6c7a5b98a1b4b41e33091745
Anthony HORNEC <ahornec@iutbayonne.univ-pau.fr>
2021-01-14 22:31:07 +0100

    Ajout des requêtes d'importation des données dans le conrôleur et passage aux vues

--- src/Controller/ProStagesController.php
+++ src/Controller/ProStagesController.php
@@ -5,46 +5,48 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Stage;
+use App\Repository\StageRepository;
+use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
+use App\Entity\Formation;
+use App\Repository\FormationRepository;
 
 class ProStagesController extends AbstractController
 {
     /**
-       * @Route("/", name="accueil")
+     * @Route("/", name="accueil")
      */
-    public function index(): Response
+    public function index(StageRepository $repositoryStage): Response
     {
-        return $this->render('pro_stages/index.html.twig', [
-            'controller_name' => 'Bienvenue sur la page d\'accueil de Prostages',
-        ]);
+        $stages = $repositoryStage->findAll();
+
+        return $this->render('pro_stages/index.html.twig', ['stageList' => $stages]);
     }
 
     /**
        * @Route("/entreprises", name="entreprises")
      */
-    public function entreprises(): Response
+    public function entreprises(EntrepriseRepository $repositoryEntreprise): Response
     {
-        return $this->render('pro_stages/entreprises.html.twig', [
-            'controller_name' => 'Cette page affichera la liste des entreprises proposant un stage',
-        ]);
+        $entreprises = $repositoryEntreprise->findAll();
+        return $this->render('pro_stages/entreprises.html.twig', ['entrepriseList' => $entreprises]);
     }
 
     /**
        * @Route("/formations", name="formations")
      */
-    public function formations(): Response
+    public function formations(FormationRepository $repositoryFormation): Response
     {
-        return $this->render('pro_stages/formations.html.twig', [
-            'controller_name' => 'Cette page affichera la liste des formations de l\'IUT',
-        ]);
+        $formations = $repositoryFormation->findAll();
+        return $this->render('pro_stages/formations.html.twig', ['formationList' => $formations]);
     }
 
     /**
        * @Route("/stages/{idStage}", name="presentationStage")
      */
-    public function stages($idStage): Response
+    public function stages(Stage $stage): Response
     {
-        return $this->render('pro_stages/presentationStage.html.twig', [
-            'idStage' => $idStage,
-        ]);
+        return $this->render('pro_stages/presentationStage.html.twig', ['stage' => $stage]);
     }
 }
